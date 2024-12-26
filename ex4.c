326930720259d5a6947a409742d818e226507a50
DvirAgassi <agassidvir@gmail.com>
2024-12-26 18:09:47 +0200

    Update ex4.c

--- ex4.c
+++ ex4.c
@@ -7,6 +7,8 @@ Assignment:
 #include <string.h>
 
 #define NUM_OF_COLUMNS 5
+#define MAX_ARRAY_PAREN 100
+#define FIRST_PLACE_IN_ARRAY 0
 
 void task1RobotPaths();
 int robot_path(int row, int column);
@@ -15,7 +17,7 @@ void task2HumanPyramid();
 float calculate_weight(float human_pyramid[NUM_OF_COLUMNS][NUM_OF_COLUMNS], int row, int column);
 
 void task3ParenthesisValidator();
-int validate_parent(char validate[100], int current_place, char end_char);
+int validate_parent(char validate[], char paren_array[], int i, int current_place);
 
 void task4QueensBattle();
 void task5CrosswordGenerator();
@@ -139,53 +141,69 @@ float calculate_weight(float human_pyramid[NUM_OF_COLUMNS][NUM_OF_COLUMNS], int 
 
 void task3ParenthesisValidator()
 {
-    // random string length idk
-    char validate[100];
+    // random big string length idk
+    char validate[MAX_ARRAY_PAREN]; // the string the user entered
+    char paren_array[MAX_ARRAY_PAREN] = {0}; // array to put all the start paren to check matches
+    printf("Please enter a term for validation:\n");
     // enter the string the user entered inside the char array with '\0' at the end to show wehre is the end of it
-    for (int i = 0; i < 99; i++) {
-        if (scanf("%c", validate[i] == '\n')) {
-            validate[i] = '\0';
-            break;
-        }
-    }
+    scanf("%s", validate);
     
     // check the array from start till you find '\0'
-    if (validate_parent(validate, 0, '\0')) {
-      printf("good");
-    }
+    // we have 2 FIRST_PLACE_IN_ARRAY. first one is for the string array the user gave, second one to see current place in paren array
+    if (validate_parent(validate, paren_array, FIRST_PLACE_IN_ARRAY, FIRST_PLACE_IN_ARRAY))
+      printf("The parentheses are balanced correctly.\n");
     else
-      printf("bad");
+      printf("The parentheses are not balanced correctly.\n");
 }
 
-int validate_parent(char validate[100], int current_place, char end_char) {
-  if (validate[current_place] == '\0') {
-        // check if the wanted end_char is '\0' (we change the end_char in the middle)
-        return end_char == '\0';
+int validate_parent(char validate[], char paren_array[], int i, int current_place) {
+    // check until you found '\0' - end of string
+    if (validate[i] == '\0') {
+        // the array of parentheses_checking is empty
+        return current_place == 0;
     }
 
-  char current_char = validate[current_place];
+    char current_char = validate[i];
 
-    // check if the char is a start parenthese, an end one, or smt else
-  switch (current_char) {
-      // if it is start of paren, check for the matching end
+    switch (current_char) {
+        // add open paren to the paren array
         case '(':
-            if (!validate_parent(validate, current_place + 1, ')')) return 0;
-            break;
         case '[':
-            if (!validate_parent(validate, current_place + 1, ']')) return 0;
-            break;
         case '{':
-            if (!validate_parent(validate, current_place + 1, '}')) return 0;
-            break;
         case '<':
-            if (!validate_parent(validate, current_place + 1, '>')) return 0;
-            break;
-        case ')': case ']': case '}': case '>':
-            // if it is the end one, check if it is matching the start
-            return current_char == end_char && validate_parent(validate, current_place + 1, '\0');
+            if (current_place == MAX_ARRAY_PAREN) {
+                // last char is open paren and it will be false cause no closer
+                return 0;
+            }
+            paren_array[current_place] = current_char;
+            // call recursive but check next char, and let it be next place in paren array
+            return validate_parent(validate, paren_array, i + 1, current_place + 1);
+
+        // check for the match
+        case ')':
+        case ']':
+        case '}':
+        case '>':
+            if (current_place == 0) {
+                return 0; // this is the first char and it wont match anything
+            }
+        
+            // find the matching to close paren
+            char match_paren = 
+                (current_char == ')') ? '(' : 
+                (current_char == ']') ? '[' : 
+                (current_char == '}') ? '{' : '<';
+            // the recursive called for next place in paren array, so we need to check match in last place
+            if (paren_array[current_place - 1] != match_paren) {
+                return 0;
+            }
+        
+            // if we got here it means there is a match, check for next char but with last place in array ("delete last char")
+            return validate_parent(validate, paren_array, i + 1, current_place - 1);
+        
+        // smt that is not a paren, just skip them and move to the next char
         default:
-            // if it is non-paren, skip to the next character
-            return validate_parent(validate, current_place + 1, end_char);
+            return validate_parent(validate, paren_array, i + 1, current_place);
     }
 }
 
